ffabe8300ceb7f8c9f10798359c2918f86f945ff
SeiperLu <62956359+SeiperLu@users.noreply.github.com>
2025-08-12 01:26:10 +0200

    Topic: Unit testing with JUnit

---
+++ Java Problems/Topics/Unit testing with JUnit/Test driven development/src/Main.java
@@ -0,0 +1,26 @@
+class Person {
+    // Do not change these fields
+    public static final String DEFAULT_NAME = "Unknown";
+    public static final int MAX_AGE = 130;
+    public static final int MIN_AGE = 0;
+    private String name;
+    private int age;
+
+    // Fix the constructor to make its code pass the unit tests
+    Person(String name, int age) {
+        this.name = name;
+        switch (age){
+          case
+        }
+
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
